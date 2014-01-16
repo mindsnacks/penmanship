0e75bc5aa147784d2e697e508b97db7ac99dfd2b
Tony Cosentini <tony@mindsnacks.com>
2014-01-16 13:17:49 -0800

    Starting to add an example project.

---
+++ markdroid-example/src/main/java/com/markdroid/example/MainActivity.java
@@ -0,0 +1,15 @@
+package com.markdroid.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tonyc on 1/16/14.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ markdroid-example/src/main/java/com/markdroid/example/Markdroid.java
@@ -0,0 +1,12 @@
+package com.markdroid.example;
+
+import android.content.Context;
+
+/**
+ * Created by tonyc on 1/16/14.
+ */
+public class Markdroid {
+  public static int getLayoutId(final String name, final Context context) {
+    
+  }
+}
